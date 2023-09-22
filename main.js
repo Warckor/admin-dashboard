9058940d2fc33f5cfc79adfdf7b4a43770c457df
Warckor <mik.el.loco@gmail.com>
2023-09-23 00:14:20 +0200

    Add theme js functionallity

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
+const themeSelector = document.getElementById('selector');
 const root = document.documentElement;
+const themeName = document.querySelector('.theme-name');
+
+/**
+ * Sets the theme of the application.
+ */
+function setTheme() {
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+/**
+ * Updates the theme name based on the current theme class.
+ */
+function changeThemeName() {
+    const newName = root.className === 'light' ? 'Light' : 'Dark';
+    themeName.textContent = newName;
+}
+
+themeSelector.addEventListener('click', () => {
+    setTheme();
+    changeThemeName();
+});
+
+window.onload = () => {
+    root.className = 'dark';
+}
